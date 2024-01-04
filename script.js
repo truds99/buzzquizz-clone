0c6b310d62988aee64a9a0af425b513376b11bfd
truds99 <dudscardoso9@gmail.com>
2024-01-04 03:12:17 -0300

    + quizz screen

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const urlAPI = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
-const yourQuizzes = document.querySelector(".yourQuizzes");
-const quizzes = document.querySelector(".allQuizzes");
+const main = document.querySelector(".main");
 
 function getQuizzes() {
     const promise = axios.get(`${urlAPI}quizzes`);
@@ -8,22 +7,79 @@ function getQuizzes() {
 }
 
 function renderQuizzes(response) {
-    const idQuizz = localStorage.getItem("id");
-    console.log(idQuizz);
-    if(!idQuizz) {
-        yourQuizzes.innerHTML = `
-            <p>You haven't created any<br/>quizzes yet :(</p>
-            <button>Create Quizz</button>`
+    main.innerHTML = `
+        <div class="mainScreen">
+            <h2 class="titleYourQuizzes">Your Quizzes</h2>
+            <div class="yourQuizzes"></div>
+            <h2>All Quizzes</h2>
+            <div class="allQuizzes"></div>
+        </div>`;
+    const yourQuizzes = document.querySelector(".yourQuizzes");
+    const quizzes = document.querySelector(".allQuizzes");
+    const savedQuizzesString = localStorage.getItem("ids");
+    const titleYourQuizzes = document.querySelector(".titleYourQuizzes");
+    if(!savedQuizzesString){
+        savedQuizzesString = "[]";
     }
+    if(savedQuizzesString === "[]"){
+        titleYourQuizzes.innerHTML = "";
+        yourQuizzes.innerHTML = `
+            <div class="noQuizz">
+                <p>You haven't created any<br/>quizzes yet :(</p>
+                <button>Create Quizz</button>
+            </div>`
+    }
+    const savedQuizzes = JSON.parse(savedQuizzesString);
     for (let i=0; i<response.data.length; i++) {
+        if(savedQuizzes.includes(response.data[i].id)){
+            yourQuizzes.innerHTML += `
+                <div class="quizz" onclick="getOnlyQuizz(${response.data[i].id})">
+                    <img src="${response.data[i].image}">
+                    <h3>${response.data[i].title}</h3>
+                    <div class="gradient"></div>    
+                </div>`
+        }
+        else{
         quizzes.innerHTML += `
-            <div class="quizz">
+            <div class="quizz" onclick="getOnlyQuizz(${response.data[i].id})">
                 <img src="${response.data[i].image}">
                 <h3>${response.data[i].title}</h3>
                 <div class="gradient"></div>    
             </div>`
+        }
     }
 }
+
+function getOnlyQuizz (quizz) {
+    const promise = axios.get(`${urlAPI}quizzes/${quizz}`);
+    promise.then(openQuizz);
+}
+
+function openQuizz(quizz) {
+    main.innerHTML= `
+        <div class="title">
+            <img src="${quizz.data.image}" alt="${quizz.data.title}">
+            <h4>${quizz.data.title}</h4>
+            <div class="blackLay"></div>
+        </div>
+        <div class="allQuestions"></div>`;
+    const allQuestions = document.querySelector(".allQuestions");
+    for (let i=0; i<quizz.data.questions.length; i++) {
+        allQuestions.innerHTML += `
+            <div class="question">
+                <h5>${quizz.data.questions[i].title}</h5>
+                <div class="allAnswers"></div>
+            </div>`;
+        const allAnswers = allQuestions.lastChild.querySelector(".allAnswers");
+        for (let j=0; j<quizz.data.questions[i].answers.length; j++) {
+            allAnswers.innerHTML += `
+                <div class="answer" class="${quizz.data.questions[i].answers[j].isCorrectAnswer}">
+                    <img src="${quizz.data.questions[i].answers[j].image}">
+                    <h6>${quizz.data.questions[i].answers[j].text}</h6>
+                </div>`
+        }
+    }
+}   
 
 getQuizzes();
 
